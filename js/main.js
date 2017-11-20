66601d80c8fcbff9456c35ce6df42b9ec117a82f
Richard Spence <richardspence@Richards-MacBook-Air.local>
2017-11-20 15:59:36 -0800

    First Commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,45 @@
-//First create a function named init and put your previous code in the function
-function init (){
-var planet = document.getElementById('greenplanet');
-planet.innerHTML = "Red Alert: hit by phaser fire";
+/*Now you might think that dealing with NaN is a rare event, but if you’re working with any kind of code that uses numbers, you’d be surprised how often it
+shows up. The most common thing you’ll need to do is test for NaN, and given everything you’ve learned about JavaScript, how to do this might seem obvious:
+*/
+
+if (myNum == NaN){
+  myNum = 0; //they aren't equal
 }
 
-window.onload = init;//Assigns the function init to the window.onload property. DO NOT
-                      //use parenthese after the function name. We aren't called the function
-                      //we are just assigning the function value to the window.onload property.
+
+/* Any sensible person would assume that’s how you test to see if a variable holds a NaN value, but it doesn’t work.
+Why? Well, NaN isn’t equal to anything, not even itself, so, any kind of test for equality with NaN is off the table.
+Instead you need to use a special function: isNaN. Like this:
+*/
+if (isNaN(myNum)){
+  myNum = 0;//Use the isNaN function, which returns true if the value passed is not a  number
+}
+
+/*So, let’s think through this a bit more. If NaN stands for “Not a Number”, what is it?
+Wouldn’t it be easier if it were named for what it is rather than what it isn’t?
+What do you think it is? We can check its type for a hint:
+*/
+
+var test11 = 0/0;
+console.log(typeof test11); //returns 'number'
 
 
+/*What on earth? NaN is of type number? How can something that’s not a number have the type number?
+Okay, deep breath. Think of NaN as just a poorly named value.
+*/
 
-//How to set an attribute with setAttribute
-planet.setAttribute('class',"redtext");//the method takes two arguments, the name of the attribute you want to set or change.
+/*Try adding the code below
+to the <script> element in a basic web page and see what you get in the console when you load up the page.
+You won’t get why yet, but see if you can take a guess about what might be going on.
+*/
+
+if (99 == "99") {
+console.log("A number equals a string!");
+} else {
+console.log("No way a number equals a string"); }  //returns 'A number equals a string'
 
 
-//How to GET attributes
-var scoop = document.getElementById('raspberry');
-var altText = scoop.getAttribute('alt');
-console.log('I can"t see the image in the console');
-console.log('but I"m told it looks like' + altText);
+if (99 === "99") {
+console.log("A number equals a string!");
+} else {
+console.log("No way a number equals a string"); } //returns 'No way a number equals a string'
